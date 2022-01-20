22694462279cdf3b9dfb8d6b17e66a844c87be4b
Amanda Leite <mandirleite@gmail.com>
2022-01-20 18:49:17 -0300

    arquivo routes de pessoas criado

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,14 @@
 const bodyParser = require("body-parser");
 
+const pessoas = require("./pessoasRoute");
+
 module.exports = app => {
     app.use(bodyParser.json());
 
-    app.get('/', (req, res) =>
-        res
-        .status(200)
-        .send({ mensagem: 'uhul' }
-        ))
+    app.use(pessoas);
+
+   
+    
+
 
 }
