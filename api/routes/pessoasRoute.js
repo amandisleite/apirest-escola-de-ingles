7d3dcb4ae451411f8d2183f2aa52f77c1d893be2
Amanda Leite <mandirleite@gmail.com>
2022-01-27 15:32:45 -0300

    busca de matrículas por turma e só trazendo aluno ativo feita

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -11,8 +11,9 @@ router.put('/pessoas/:id', PessoaController.atualizaPessoa);
 router.delete('/pessoas/:id', PessoaController.apagaPessoa);
 router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa);
 
-router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas);
+router.get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma);
 
+router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas);
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
 router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
 router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
