abd784818d9c30ec4de182cb1ef0ba4e42758f23
Amanda Leite <mandirleite@gmail.com>
2022-01-27 15:20:03 -0300

    consulta só por aluno ativo e consulta de turmas com filtro de data inicial e final feitos

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -14,12 +14,21 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'docente_id'
       })
       Pessoas.hasMany(models.Matriculas, {
-        foreignKey: 'estudante_id'
+        foreignKey: 'estudante_id',
+        scope: { status: 'confirmado' },
+        as: 'aulasMatriculadas'
       })
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        tamanhoMinimoDoNome: function(dado) {
+          if (dado.length < 3) throw new Error('o campo nome deve ter mais de três caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     email: {
       type: DataTypes.STRING,
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -11,10 +11,12 @@ router.put('/pessoas/:id', PessoaController.atualizaPessoa);
 router.delete('/pessoas/:id', PessoaController.apagaPessoa);
 router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa);
 
+router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas);
+
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
 router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
 router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
-router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula);
 router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula);
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula);
 
 module.exports = router;
